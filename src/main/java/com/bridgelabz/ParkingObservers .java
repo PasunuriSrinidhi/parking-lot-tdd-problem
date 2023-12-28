71e88b7c601a3b4c40dc394de3b1728ba8045179
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-28 23:22:50 +0530

    Added: Notify Manager about Full Parking Space

---
+++ src/main/java/com/bridgelabz/ParkingObservers .java
@@ -0,0 +1,10 @@
+/*
+ * @Description -Notify the observers about parking lot status
+ * 
+ * @Methods - update() - notifies the observers about parking lot status
+ */
+public interface ParkingObservers {
+	void update();
+
+	String getStatus();
+}

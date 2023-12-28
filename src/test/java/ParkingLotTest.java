f8bcd1d4f3684735e3124c573e65d5e675bfe84d
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-28 23:27:49 +0530

    Added: Notify Owner about Full Parking Space

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,35 +1,108 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import com.bridgelabz.Car;
+import com.bridgelabz.ParkingLot;
+import com.bridgelabz.ParkingOwner;
+import com.bridgelabz.ParkingObservers;
 
-import com.bridgelabz.model.Car;
-import com.bridgelabz.model.ParkingLot;
+/*
+ * @Description - Test cases for ParkingLot class
+ * 
+ * @Properties - parkingLot - object of ParkingLot class
+ * 				owner - object of ParkingOwner class
+ * 				car - object of Car class
 
+ * @Methods - testParkCars() - tests the parkCar() method
+ * 			 testUnparkCars() - tests the unparkCar() method
+ * 			 testNotifyObservers() - tests the notifyObservers() method
+ * 
+ */
 class ParkingLostTest {
 	ParkingLot parkingLot;
+	ParkingObservers owner;
+	Car car1;
+	Car car2;
 
+
+	/*
+	 * @Description - sets up the objects for testing
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
-		parkingLot = new ParkingLot(100);
+		parkingLot = new ParkingLot(3);// Create a parking lot with capacity 3
+		owner = new ParkingOwner();// Create a parking owner
+		parkingLot.addObservers(manager);// Add the owner to the list of observers
 
-	}
+		// Create two cars
+		System.out.println();
+		car1 = new Car("MH-12-1234", "BMW", "White");
+		car2 = new Car("MH-12-1235", "Audi TT", "Goodwood Green Pearl");
 
-	@Test
-	void givenACar_AddToParkingLot_ReturnBoolean() {
-		Car car1 = new Car("MH-12-1234", "BMW", "White");
-		Car car2 = new Car("MH-12-1235", "Audi TT", "Goodwood Green Pearl");
+		// Park the cars
 		parkingLot.parkCar(car1);
 		parkingLot.parkCar(car2);
-                parkingLot.printParkedCars();
-		assertEquals(2, parkingLot.getParkedCars().size());
+
 	}
 
-       @Test
-	void givenACar_RemoveFromParkingLot_Returnhome() {
+	/*
+	 * @Description - tests the parkCar() method
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
+	@Test
+	void testParkCars() {
+		System.out.println("Test Park Cars");
+		Car car3 = new Car("MH-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray");
+		Car car4 = new Car("MH-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver");
+
+		parkingLot.parkCar(car3);
+		parkingLot.parkCar(car4);
+		assertEquals(3, parkingLot.getParkedCars().size());
+		System.out.println();
+	}
+
+	/*
+	 * @Description - tests the unparkCar() method
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
+	@Test
+	void testUnparkCars() {
+		System.out.println("Test Unpark Cars");
 		parkingLot.unparkCar("MH-12-1234");
 
-        assertFalse(parkingLot.getParkedCars().contains(car1));
-        assertTrue(parkingLot.getParkedCars().contains(car2));
+		assertFalse(parkingLot.getParkedCars().contains(car1));
+		assertTrue(parkingLot.getParkedCars().contains(car2));
+		System.out.println();
+	}
+
+	/*
+	 * @Description - tests the notifyObservers() method
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
+	@Test
+	void testNotifyObservers() {
+		System.out.println("Test Notify Observers");
+		Car car3 = new Car("MH-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray");
+		Car car4 = new Car("MH-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver");
+
+		parkingLot.parkCar(car3);
+		parkingLot.parkCar(car4);
+
+		assertEquals("full", manager.getStatus());
+		System.out.println();
+		
 	}
 }

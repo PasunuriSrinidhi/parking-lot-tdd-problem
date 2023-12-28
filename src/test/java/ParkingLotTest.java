1e0176f84eee41d0511ddca2e7beb7fff289176a
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-28 23:03:39 +0530

    Testing parked cars

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ParkingLotTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.bridgelabz.model.Car;
+import com.bridgelabz.model.ParkingLot;
+
+class ParkingLostTest {
+	ParkingLot parkingLot;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		parkingLot = new ParkingLot(100);
+
+	}
+
+	@Test
+	void givenACar_AddToParkingLot_ReturnBoolean() {
+		Car car1 = new Car("MH-12-1234", "BMW", "White");
+		Car car2 = new Car("MH-12-1235", "Audi TT", "Goodwood Green Pearl");
+
+		parkingLot.parkCar(car1);
+		parkingLot.parkCar(car2);
+
+		parkingLot.printParkedCars();
+
+		assertEquals(2, parkingLot.getParkedCars().size());
+	}
+
 }
